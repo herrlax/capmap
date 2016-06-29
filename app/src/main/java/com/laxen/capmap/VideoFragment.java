d4d929803c08840c0aaf39839b14781ae967ef74
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2016-06-29 19:41:26 +0200

    center video in landscape mode

--- app/src/main/java/com/laxen/capmap/VideoFragment.java
+++ app/src/main/java/com/laxen/capmap/VideoFragment.java
@@ -7,6 +7,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;

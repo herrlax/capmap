a2500c1cf1fd5a9f63936251a4ec7662310c47cf
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2016-07-11 19:01:21 +0200

    Add card view for my videos

---
+++ app/src/main/java/com/laxen/capmap/tabs/ListFragmentAdapter.java
@@ -0,0 +1,90 @@
+package com.laxen.capmap.tabs;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.laxen.capmap.R;
+import com.laxen.capmap.utils.VideoItem;
+
+import java.util.List;
+
+/**
+ * Created by laxen on 7/11/16.
+ */
+public class ListFragmentAdapter extends RecyclerView.Adapter<ListFragmentAdapter.ViewHolder> {
+
+    private List<VideoItem> videos;
+    private Context context;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public ListFragmentAdapter(List<VideoItem> videos, Context context) {
+        this.context = context;
+        this.videos = videos;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.video_card, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final VideoItem videoItem = videos.get(position);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo display video from videoItem
+            }
+        });
+
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo remove video
+            }
+        });
+
+        holder.timeStampTextView.setText("placeholder timestamp");
+        holder.locationTextView.setText("Long: " + videoItem.getLongitude()
+                                        + ", Lat: " + videoItem.getLatitude());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return videos.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView timeStampTextView;
+        protected TextView locationTextView;
+        protected TextView viewsTextView;
+        protected ImageButton deleteButton;
+        protected CardView cardView;
+        protected View cardBackground;
+
+        public View view;
+        public ViewHolder(View v) {
+            super(v);
+            timeStampTextView = (TextView) v.findViewById(R.id.timeStampTextView);
+            locationTextView = (TextView) v.findViewById(R.id.locationTextView);
+            viewsTextView = (TextView) v.findViewById(R.id.viewsTextView);
+            deleteButton = (ImageButton) v.findViewById(R.id.deleteButton);
+            cardView = (CardView) v.findViewById(R.id.card_view);
+            cardBackground = v.findViewById(R.id.cardBackground);
+
+        }
+    }
+}
--- app/src/main/java/com/laxen/capmap/tabs/ListFragmentTab.java
+++ app/src/main/java/com/laxen/capmap/tabs/ListFragmentTab.java
@@ -1,9 +1,49 @@
 package com.laxen.capmap.tabs;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laxen.capmap.R;
+import com.laxen.capmap.utils.VideoItem;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by laxen on 7/10/16.
  */
 public class ListFragmentTab extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_myvideos,container,false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true); // for improved performance
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        List<VideoItem> dummyData = new LinkedList<>();
+        dummyData.add(new VideoItem("23.4", "32.2", ""));
+        dummyData.add(new VideoItem("64.4", "86.2", ""));
+        dummyData.add(new VideoItem("27.4", "12.2", ""));
+
+        mAdapter = new ListFragmentAdapter(dummyData, getContext());
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        return view;
+    }
+
 }
